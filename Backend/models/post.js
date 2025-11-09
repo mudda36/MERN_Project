c100a3bda20cfea8f8462f33b589fc762f125f48
mudda36 <muddappa130@gmail.com>
2025-11-09 21:49:37 +0530

    Add files via upload

---
+++ Backend/models/post.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const postSchema = mongoose.Schema({
+    author:{
+        type:String,
+        required:true
+    },
+    title:{
+        type:String,
+        required:true
+    },
+    summary:{
+        type:String,
+        required:true
+    },
+    image:{
+        type:String,
+        required:true
+    },
+    location:{
+        type:String,
+        required:true
+    }
+})
+module.exports = mongoose.model('Post',postSchema);
